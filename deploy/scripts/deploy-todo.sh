dc3d4d0bba560783ff8b239d0d0fb37a91816711
Daniel4617pm <daniel.parralesmerino4617@upse.edu.ec>
2026-07-27 13:45:48 -0500

    Corregir permisos del usuario de despliegue

--- deploy/scripts/deploy-todo.sh
+++ deploy/scripts/deploy-todo.sh
@@ -11,6 +11,10 @@ test -f .env
 DATABASE_URL="$(/usr/bin/sed -n 's/^DATABASE_URL=//p' .env | /usr/bin/tail -n 1)"
 test -n "$DATABASE_URL"
 /usr/bin/psql "$DATABASE_URL" -f database/init.sql
-/bin/chown -R todoapp:todoapp "$APP_DIR"
+/bin/chown -R deployer:todoapp "$APP_DIR"
+/usr/bin/find "$APP_DIR" -type d -exec /bin/chmod 750 {} \;
+/usr/bin/find "$APP_DIR" -type f -exec /bin/chmod 640 {} \;
+/bin/chown todoapp:todoapp "$APP_DIR/.env"
+/bin/chmod 600 "$APP_DIR/.env"
 /bin/systemctl restart todo-cicd
 /bin/systemctl is-active --quiet todo-cicd
