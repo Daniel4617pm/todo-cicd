7435005e66bc6e576a71fe5ee09510bb1db38b72
Daniel4617pm <daniel.parralesmerino4617@upse.edu.ec>
2026-07-27 13:12:07 -0500

    Automatizar despliegue de base de datos

--- deploy/scripts/deploy-todo.sh
+++ deploy/scripts/deploy-todo.sh
@@ -8,6 +8,9 @@ test -f .env
 
 /usr/bin/npm ci --omit=dev
 /usr/bin/npm run check
+DATABASE_URL="$(/usr/bin/sed -n 's/^DATABASE_URL=//p' .env | /usr/bin/tail -n 1)"
+test -n "$DATABASE_URL"
+/usr/bin/psql "$DATABASE_URL" -f database/init.sql
 /bin/chown -R todoapp:todoapp "$APP_DIR"
 /bin/systemctl restart todo-cicd
 /bin/systemctl is-active --quiet todo-cicd
